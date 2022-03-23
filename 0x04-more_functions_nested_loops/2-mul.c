7f8bcce9437025aa62576a7021b32c3e41b487f2
root <habtetsegaye19@gmail.com>
2022-03-23 07:28:11 -0700

    mul added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: int param 1
+ * @b: int param 3
+ * Return: products of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
