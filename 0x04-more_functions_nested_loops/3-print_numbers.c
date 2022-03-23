ba5a11de1adc76d30418343be4ef0dc5a030ac4b
root <habtetsegaye19@gmail.com>
2022-03-23 08:35:04 -0700

    jkdl

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9 
- * Return: the numbers beween 9-9
- */
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_numbers(void)
 {
 	int x;
