f61240ada896fbaeba3e70e0f1790e8ad6ac4abc
root <habtetsegaye19@gmail.com>
2022-03-23 08:43:29 -0700

    error fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_numbers - Print the numbers since 0 up to 9
+  * print_most_numbers - Print the numbers since 0 up to 9
   *
   * Return: The numbers since 0 up to 9
   */
