9ab25557c1a861b32aec1ea2ba4087b272d3137a
root <habtetsegaye19@gmail.com>
2022-03-23 07:36:09 -0700

    third file added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9 
+ * Return: void
+ *
+ */
+void print_numbers(void)
+{
+	for (int i = 48; i < 58; i++)
+	{
+		printf("%c", i);
+	}
+
+	printf(\n);
+}
+
