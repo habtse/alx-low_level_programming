deca241c194d0b3fb2b8435256952020acd49267
habtamu tsegaye <habte@kali.habte>
2023-06-16 15:59:48 -0400

    variable if else while added

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  * main - print digit
- * 
  * Return: 0 if success
 */
 int main(void)
 {
     int a;
+
     for (a = 0; a < 10; a++)
     {
-        printf("%d",a);
+        printf("%d", a);
     }
     printf("\n");
     return (0);
