776dcd49d28eabf69643b6e9b29d9481ea77ea49
habtamu tsegaye <habte@kali.habte>
2023-06-16 16:20:28 -0400

    variable if else while added

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,11 +5,12 @@
 */
 int main(void)
 {
-    char letter;
-    for (letter = 'z'; letter >= 'a'; letter--)
-    {
-    putchar(letter);
-    }
-    putchar('\n');
-    return (0);    
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0); 
+   
 }
