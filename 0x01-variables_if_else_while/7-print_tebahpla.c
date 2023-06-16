aafa417b4875afec8f42ce75eaf36c997448f4d1
habtamu tsegaye <habte@kali.habte>
2023-06-16 16:18:16 -0400

    variable if else while added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alphabet with reverse order
+ * Return: 0 if success
+*/
+int main(void)
+{
+    char letter;
+    for (letter = 'z'; letter >= 'a'; letter--)
+    {
+    putchar(letter);
+    }
+    putchar('\n');
+    return (0);    
+}
