b03a8957c3b4469efaf07d678277834db08f1f62
habtamu tsegaye <habte@kali.habte>
2023-06-16 15:33:49 -0400

    variable if else while added

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,26 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main - prints alphabet without q and e
- * Return: 0 if success
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	char letter;
 
-	for (letter = 'a';letter <= 'z';letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
-		{
-		putchar(letter);
-		}
+			putchar(letter);
 	}
-	
+
 	putchar('\n');
 
 	return (0);
-
 }
