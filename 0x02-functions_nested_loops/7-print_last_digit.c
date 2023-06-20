ee4a6c8911606bea4d6a2a08fc2cb4b2776d8c91
habtamu tsegaye <habte@kali.habte>
2023-06-20 16:25:42 -0400

    file 8 is added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: parameter int
+ * Return: the last value
+*/
+int print_last_digit(int n)
+{
+int i;
+i = n % 10;
+_putchar(i + '0');
+return (i);
+}
