a85397327c49c72c691e04c922829eb5aceeb751
habtamu tsegaye <habte@kali.habte>
2023-06-20 16:15:13 -0400

    file 7 is added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - returns absolute value of the number
+ * @n: int parameter
+ * Return: the absolute value 
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (-1 * n);
+else
+return (n);
+}
