d50fd476f123fadcc464cc70a5cad1c65204ef2d
habtamu tsegaye <habte@kali.habte>
2023-06-20 15:42:26 -0400

    file 5 added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - returns 1 if its alphabet otherwise returns 0
+ *  @c: prameter c int value
+ * Return:  int if success
+*/
+int isalpha(int c) 
+{
+    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+        return (1);
+    else
+        return(0);
+}
