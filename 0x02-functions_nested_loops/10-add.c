4a6dae9e82cd37b8de85d9b4f72f65bbcf807b5e
habtamu tsegaye <habte@kali.habte>
2023-06-21 04:34:02 -0400

    file 10 is added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - fuction that add two numbers
+ * @x: first parameter
+ * @y: second parameter
+ * Return: int if success
+*/
+int add(int x, int y)
+{
+int n;
+n = x + y;
+return (n);
+}
