ac1f0dcbf2886111dfe47bd061beb4c4ced52c07
habtamu tsegaye <habte@kali.habte>
2023-06-23 03:17:45 -0400

    file 8 added

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - prints square of #
+ * @size: int parameter
+ * 
+ * Return: empty
+*/
+void print_square(int size)
+{
+    int x, y;
+
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {   for (x = 0; x < size; x++)
+        {
+            for (y=0; y < size; y++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}
