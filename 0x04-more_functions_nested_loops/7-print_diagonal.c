261090e31f6260fb5370c9e318282eec3c567b26
habtamu tsegaye <habte@kali.habte>
2023-06-23 02:53:40 -0400

    file 7 added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal -prints diagonal line
+ * @n:  int parameter
+ * Description: prints diagonal
+*/
+void print_diagonal(int n)
+{
+int x;
+
+if (n >= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n-1; x++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
